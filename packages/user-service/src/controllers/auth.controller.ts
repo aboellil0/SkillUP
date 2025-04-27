71807284589b11c769c74760246fab064949bc9b
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-04-27 22:37:10 +0300

    setRefreshTokenCookie

--- packages/user-service/src/controllers/auth.controller.ts
+++ packages/user-service/src/controllers/auth.controller.ts
@@ -21,4 +21,15 @@ export class AuthController{
             res.status(400).json(error);
         }
     }
-}
+
+    private setRefreshTokenCookie(res: Response, token: string): void {
+        // Set HTTP-only cookie
+        res.cookie('refreshToken', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: 'strict',
+          maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        });
+      }
+}   
+
