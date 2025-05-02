274238533bf44e8c1e982ce67840b7b7dffa2915
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-05-02 10:36:22 +0300

    authrouter

---
+++ packages/user-service/src/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import {  Router } from 'express';
+import authController from '../controllers/auth.controller';
+
+
+const router = Router();
+
+router.post('/register',authController.registerUser.bind(authController));
+router.post('/login',authController.loginUser.bind(authController));
+router.post('/logout',authController.LogOutUser.bind(authController));
