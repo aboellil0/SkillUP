f21992a6653c9145e8dcfee6e90c384c0c0ff9e3
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-05-02 15:59:43 +0300

    some modifiaction on usercontroller

--- packages/user-service/src/controllers/user.controller.ts
+++ packages/user-service/src/controllers/user.controller.ts
@@ -12,18 +12,7 @@ export class UserController{
 
     async getCurrentUser(req:Request,res:Response):Promise<void>{
         try{
-            // verfication of token
-            const refershTokenFromCokkie = req.cookies.refreshToken;
-            const accessTokenFromHeader = req.headers.authorization?.split(" ")[1];
-            if(!refershTokenFromCokkie){
-                res.status(401).json({message:"No refresh token in cookie"});
-            }
-            const isAccessTokenValid = await tokenService.verifyAccessToken(accessTokenFromHeader as string);
-            if(!isAccessTokenValid){
-                res.status(401).json({message:"Invalid access token"});
-            }
-
-            const userId = req.params.id
+            const userId = (req as any).user.id;
             const user =  await userService.getUserById(userId)
             if(!user){
                 res.status(404).json({success:false,message:"User not found"});
@@ -47,32 +36,21 @@ export class UserController{
 
     async updateUser(req:Request,res:Response):Promise<void>{
         try{
-            // verfication of token
-            const refershTokenFromCokkie = req.cookies.refreshToken;
-            const accessTokenFromHeader = req.headers.authorization?.split(" ")[1];
-            if(!refershTokenFromCokkie){
-                res.status(401).json({message:"No refresh token in cookie"});
-            }
-            const isAccessTokenValid = await tokenService.verifyAccessToken(accessTokenFromHeader as string);
-            if(!isAccessTokenValid){
-                res.status(401).json({message:"Invalid access token"});
-            }
-
-            const userId = req.params.id
+            const userId = (req as any).user.id
             const {firstName,lastName,phoneNumber} = req.body
-            const user =  await userService.updateUSer(userId,{firstName,lastName,phoneNumber})
-            if(!user){
+            const userF =  await userService.updateUSer(userId,{firstName,lastName,phoneNumber})
+            if(!userF){
                 res.status(404).json({success:false,message:"User not found"});
             }
             res.status(200).json({success:true,data:{
                 user:{
-                    id:user?._id,
-                    name:user?.firstName + " " + user?.lastName,
-                    email:user?.email,
-                    phone:user?.phoneNumber,
-                    role:user?.role,
-                    createdAt:user?.createdAt,
-                    updatedAt:user?.updatedAt
+                    id:userF?._id,
+                    name:userF?.firstName + " " + userF?.lastName,
+                    email:userF?.email,
+                    phone:userF?.phoneNumber,
+                    role:userF?.role,
+                    createdAt:userF?.createdAt,
+                    updatedAt:userF?.updatedAt
                 }
             }});
         }catch(error){
@@ -82,19 +60,7 @@ export class UserController{
     }
     async deleteUser(req:Request,res:Response):Promise<void>{
         try{
-            // verfication of token
-            const refershTokenFromCokkie = req.cookies.refreshToken;
-            const accessTokenFromHeader = req.headers.authorization?.split(" ")[1];
-            if(!refershTokenFromCokkie){
-                res.status(401).json({message:"No refresh token in cookie"});
-            }
-            const isAccessTokenValid = await tokenService.verifyAccessToken(accessTokenFromHeader as string);
-            if(!isAccessTokenValid){
-                res.status(401).json({message:"Invalid access token"});
-            }
-            
-            
-            const userId = req.params.id
+            const userId = (req as any).user.id
             const user =  await userService.deletUser(userId)
             if(!user){
                 res.status(404).json({success:false,message:"User not found"});
