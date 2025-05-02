7878b34a2eb4506bf00702551eb6de733296be81
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-05-02 11:10:05 +0300

    server.ts

---
+++ packages/user-service/src/server.ts
@@ -0,0 +1,56 @@
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import helmet from 'helmet';
+import config from './config';
+
+import authRoutes from './routes/auth.routes';
+import userRoutes from './routes/user.routes';
+
+export class server{
+    public app:Application;
+
+    public constructor(){
+        this.app = express();
+        this.configrationMiddleware();
+        this.configreRoute();
+    }
+
+    private configrationMiddleware():void {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended:true}));
+
+        this.app.use(cors());
+        this.app.use(helmet());
+
+        this.app.use((req,res,next)=>{
+            console.log(`${new Date().toISOString()}-${req.method} ${req.path}`);
+            next();
+        });
+    }
+
+    private configreRoute():void{
+        this.app.use('/api/auth',authRoutes);
+        this.app.use('/api/users',userRoutes);
+
+
+        this.app.get('/health', (req, res) => {
+            res.status(200).json({
+              status: 'ok',
+              message: 'User service is running',
+              timestamp: new Date().toISOString()
+            });
+        });
+    }
+
+    public start():void{
+        const port =  config.port;
+
+        this.app.listen(port,()=>{
+            console.log(`server is running on port ${port}`);
+        });
+    }
+}
+
+
+export default new server();
